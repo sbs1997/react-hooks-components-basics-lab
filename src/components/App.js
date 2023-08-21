4ffda4668e2947bf4955237ebdb590d2f2b7b28f
Sky Bauerschmidt Sweeney <dbauerschmidtsweeney@gmail.com>
2023-08-21 12:13:18 -0600

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Home from "./home";
 
 function NavBar() {
   return (
@@ -8,23 +9,23 @@ function NavBar() {
   );
 }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
-{/* write an <About> component here */}
+
+function About(){
+  return (
+    <div id="about">
+    <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
